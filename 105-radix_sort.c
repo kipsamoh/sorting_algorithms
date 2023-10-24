285f20a7de40fce19ce95b843705c4244dfebb2b
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-10-24 18:34:27 +0300

    105-radix_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,25 +1,25 @@
 #include "sort.h"
 /**
- * radix_sort - sorts an array of integers in ascending
- * order using the Radix sort algorithm
+ * radix_sort - sorts an array of _integers in ascending
+ * order using the _Radix sort algorithm
  *
- * @array: input array
+ * @array: input _array
  * @size: size of the array
  */
 void radix_sort(int *array, size_t size)
 {
-	int flag = 1, n = 10;
+	int _flag = 1, n = 10;
 	size_t i, f;
 
 	if (!array || size == 1)
 		return;
-	while (flag)
+	while (_flag)
 	{
-		flag = 0;
+		_flag = 0;
 		for (i = 1, f = 1; i <  size; i++, f++)
 		{
 			if ((array[i - 1] % (n * 10)) / ((n * 10) / 10) > 0)
-				flag = 1;
+				_flag = 1;
 			if (((array[i - 1] % n) / (n / 10)) > ((array[i] % n) / (n / 10)))
 			{
 				array[i - 1] = array[i - 1] + array[i];
