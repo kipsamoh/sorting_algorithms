1a702f808a19afc4a51df3fccfbddf860905a0d7
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-10-24 18:49:22 +0300

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,57 +1,57 @@
 #include "sort.h"
 /**
- * quick_sort - function that sorts an array
- * of integers in ascending order using the
- * quick sort algorithm
+ * quick_sort - -function that _sorts an _array
+ * of integers in _ascending order using the
+ * _quick sort _algorithm
  *
- * @array: input arrray
- * @size: size of the array
- * Return: no return
+ * @array: _input arrray
+ * @size: size of the _array
+ * Return: no _return
  */
 void quick_sort(int *array, size_t size)
 {
 	_qsort(array, 0, size - 1, size);
 }
 /**
- * _qsort - auxiliar function for the
- * quick_sort function
+ * _qsort - _auxiliar function for the
+ * quick_sort _function
  * @a: input arrray
- * @low: index for the first element
- * @high: index for the last element
- * @size: size of the array
- * Return: no return
+ * @low: index for the _first element
+ * @high: index for the last _element
+ * @size: size of the _array
+ * Return: no _return
  */
 void _qsort(int *a, int low, int high, int size)
 {
-	int p, w, i;
-	int tmp;
+	int h, l, x;
+	int temp;
 
 	if (low < high)
 	{
-		p = high;
-		w = low;
-		for (i = low; i < high; i++)
+		h = high;
+		l = low;
+		for (x = low; x < high; x++)
 		{
-			if (a[i] < a[p])
+			if (a[x] < a[h])
 			{
-				if (i != w)
+				if (x != l)
 				{
-					tmp = a[i];
-					a[i] = a[w];
-					a[w] = tmp;
+					temp = a[x];
+					a[x] = a[l];
+					a[l] = temp;
 					print_array(a, size);
 				}
-				w++;
+				l++;
 			}
 		}
-		if (w != p && a[w] != a[p])
+		if (l != h && a[l] != a[h])
 		{
-			tmp = a[w];
-			a[w] = a[p];
-			a[p] = tmp;
+			temp = a[l];
+			a[l] = a[h];
+			a[h] = temp;
 			print_array(a, size);
 		}
-		_qsort(a, low, w - 1, size);
-		_qsort(a, w + 1, high, size);
+		_qsort(a, low, l - 1, size);
+		_qsort(a, l + 1, high, size);
 	}
 }
